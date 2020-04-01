06d0e489494203caafbba8534144218b113d8226
mr-andersen523 <chasea523@gmail.com>
2020-04-01 16:07:06 -0700

    deleted imports for unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import './App.css';
 
@@ -14,12 +12,6 @@ function App() {
         <Switch>
           <Route exact path={["/", "/books"]}>
             <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
           </Route>
         </Switch>
       </div>
